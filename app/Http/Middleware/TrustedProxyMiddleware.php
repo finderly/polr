114de95bd4635450591596594eb1ddcd2619eab5
XDestination <XDestination@users.noreply.github.com>
2018-09-10 16:28:25 +0200

    Supported trusted proxies

---
+++ app/Http/Middleware/TrustedProxyMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TrustedProxyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (env('SETTING_TRUSTED_PROXIES')) {
+          $request->setTrustedProxies(explode(',', env('SETTING_TRUSTED_PROXIES')));
+        }
+        return $next($request);
+    }
+}
